6d1918d2ac931e14c777e90b727d5b783fee3388
laconal <asliddin.forwork@gmail.com>
2026-09-12 00:19:57 +0500

    fixed

--- src/core/celery/celeryApp.py
+++ src/core/celery/celeryApp.py
@@ -10,6 +10,7 @@ celery_app = Celery(
 
 celery_app.conf.update(
     timezone="UTC",
+    task_ignore_result=True,
     beat_schedule={
         "poll-notifications-every-minute": {
             "task": "poll_and_deliver_notification",
